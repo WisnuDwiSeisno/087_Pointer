0f4eab924c29a09282cd5a518bc3bba3498dbbe3
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-10 11:16:35 +0700

    selesai membuat class orang dan siswa

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -4,5 +4,17 @@ using namespace std;
 class siswa;
 
 class orang {
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
 
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
 };
